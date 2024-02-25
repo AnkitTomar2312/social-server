32ad5b2a7e5688385fc409c9932b3bb647e4e708
Ankit Tomar <ankitjdec@gmail.com>
2024-02-25 11:45:09 +0530

    setted the folder structure

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express");
-
+const Config = require("./config/config");
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("working");
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(Config.PORT, () => {
+  console.log(`Server is running on port ${Config.PORT}`);
 });
