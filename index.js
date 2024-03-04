47b0b2c77babcb407b87f20aae75c29b55902827
Ankit Tomar <ankitjdec@gmail.com>
2024-03-04 20:10:21 +0530

    added the cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
 const Config = require("./config/config");
 const userRoutes = require("./routes/user.routes");
@@ -8,7 +9,12 @@ const postRoutes = require("./routes/post.routes");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 app.use("/", userRoutes);
 app.use("/", authRoutes);
 app.use("/", postRoutes);
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,7 +2,9 @@ const express = require("express");
 const userCtrl = require("../controllers/user.controller");
 const router = express.Router();
 const authMiddleware = require("../middlewares/auth.middleware");
+
 router.route("/api/users/register").post(userCtrl.createUser);
+
 router.route("/api/users").get(userCtrl.getallusers);
 
 router.put(
